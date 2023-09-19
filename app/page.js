af3a1eb18a59765df67d16de5b6311601133f0b4
Hartaj Singh Mann <99726645+HartajMann@users.noreply.github.com>
2023-09-19 14:29:48 -0600

    week2 Page complete h1 tag still not working.

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import StudentInfo from "../app/StudentInfo.js";
+import Link from "next/link";
 
 export default function Page()
 {
@@ -6,6 +7,7 @@ export default function Page()
     <div>
       <h1> CPRG 306: Web Development 2 - Assignments</h1>
         <StudentInfo/>
+        <Link href="/week2">Week 2</Link>
     </div>
   );
 }
