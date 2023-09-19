c5cfd101567f724b08aa25b15f283a63d1543e65
Hartaj Singh Mann <99726645+HartajMann@users.noreply.github.com>
2023-09-19 14:14:14 -0600

    First commit H1 tag not working

---
+++ app/StudentInfo.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function StudentInfo()
+{   return(
+    <div>
+        <p>Name: Hartaj Singh Mann</p>
+        <p>Course section: CPRG 306 A</p>
+        <Link href="https://github.com/">https://github.com/</Link>
+    </div>
+);
+}
