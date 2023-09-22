4d75c4326d9b44ea7a3e7fe54aab589fe3194674
Hartaj Singh Mann <99726645+HartajMann@users.noreply.github.com>
2023-09-22 17:20:10 -0600

    Week 3 Assignment complete

--- app/page.js
+++ app/page.js
@@ -7,7 +7,8 @@ export default function Page()
     <div>
       <h1> CPRG 306: Web Development 2 - Assignments</h1>
         <StudentInfo/>
-        <Link href="/week2">Week 2</Link>
+        <p><Link href="/week2">Week 2</Link></p>
+        <p><Link href="/week3">Week 3</Link></p>
     </div>
   );
 }
